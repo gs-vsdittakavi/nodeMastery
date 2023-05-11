f34e1e75092df0b7c4b21eb2d9d52fdd68e7b33a
vsdittavi <vsdittakavi@gainsight.com>
2023-05-11 21:19:06 +0530

    feat(login): implemented login and auth token

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userRoute =  express.Router();
 
@@ -8,7 +9,7 @@ const User = require('../model/user');
 userRoute.post('/register',(req, res) => {
 
     const userData = req.body;
-
+    // encrypting the password
     bcrypt.hash(userData.password, 10)
     .then((encryptedPassword) => {
         const user = new User({
@@ -30,6 +31,50 @@ userRoute.post('/register',(req, res) => {
     }).catch(err => {
         console.log("error while encrypting", err);
     })
+})
+
+
+userRoute.post('/login', (req, res) => {
+    const userData = req.body;
+    user.findOne({email: userData.email}).then(user => {
+        if(user) {
+            return bcrypt.compare(userData.password, user.password).then(authStatus => {
+                if(authStatus) {
+                    console.log(authStatus);
+                    return jwt.sign(
+                        {
+                            email: user.email, 
+                            id: user._id
+                        }, 
+                        '10x_academy_node_mastery',  // secret password-> store it securely somewhere
+                        {
+                        expiresIn: "1h"
+                        }, (err, token) => {
+                            if(err) {
+                                return res.json({
+                                    message: "Token creation failed"
+                                });
+                            }
+                            return res.json({
+                                message: "Authentication successful",
+                                token: token
+                            });
+                        }
+                    )
+                }
+                res.status(401).json({
+                    message: "Authentication failed"
+                });
+
+            })
+        }
+        res.status(401).json({
+            message: "Authentication failed"
+        });
+    }).catch(err => {
+        res.send(err);
+    })
+
 })
 
 
