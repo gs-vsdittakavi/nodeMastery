689d3dee9187b9df23077541933393107daadad7
vsdittavi <vsdittakavi@gainsight.com>
2023-05-14 17:24:25 +0530

    test(unit): jest and supertest

---
+++ test/sample.test1.js
@@ -0,0 +1,28 @@
+const add = require('./sample');
+
+
+describe('to test add functionality',() => {
+
+    it('should add two numbers and give correct output for valid input', () => {
+        
+        let result = add(2,3);
+        expect(add).toBeDefined();
+        // expect(add).toHaveBeenCalledWith(2,3);
+        expect(result).toBe(5);
+    });
+
+
+    it('should work for string numbers', () => {
+        const result = add('2','3');
+        expect(result).toBe(5);
+    });
+
+    test('should fail for strings other than numbers', () => {
+        const result = add('test','fail');
+        expect(result).toBe("please provide valid numbers")
+    });
+
+
+
+
+});
